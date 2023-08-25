634b073c39b0382b3aa4ab0e6be0d9b1ff221da3
Ewen-Zippedscript <ewen@zippedscript.com>
2023-08-25 22:15:20 +0100

    Updated project info

--- setup.py
+++ setup.py
@@ -6,27 +6,27 @@ from setuptools import setup
 with open(Path("fastapi_sqlalchemy") / "__init__.py", encoding="utf-8") as fh:
     version = re.search(r'__version__ = "(.*?)"', fh.read(), re.M).group(1)
 
-with open("README.rst", encoding="utf-8") as fh:
+with open("README.md", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="FastAPI-SQLAlchemy",
+    name="FastAPI-SQLAlchemy-enhanced",
     version=version,
-    url="https://github.com/mfreeborn/fastapi-sqlalchemy",
+    url="https://github.com/Ewen-Zippedscript/fastapi-sqlalchemy",
     project_urls={
-        "Code": "https://github.com/mfreeborn/fastapi-sqlalchemy",
-        "Issue tracker": "https://github.com/mfreeborn/fastapi-sqlalchemy/issues",
+        "Code": "https://github.com/Ewen-Zippedscript/fastapi-sqlalchemy",
+        "Issue tracker": "https://github.com/Ewen-Zippedscript/fastapi-sqlalchemy/issues",
     },
     license="MIT",
-    author="Michael Freeborn",
-    author_email="michaelfreeborn1@gmail.com",
-    description="Adds simple SQLAlchemy support to FastAPI",
+    author="Ewen Lorimer",
+    author_email="ewen@zippedscript.com",
+    description="Adds simple SQLAlchemy support with multiple databases to FastAPI.",
     long_description=long_description,
     packages=["fastapi_sqlalchemy"],
     package_data={"fastapi_sqlalchemy": ["py.typed"]},
     zip_safe=False,
     python_requires=">=3.7",
-    install_requires=["starlette>=0.12.9", "SQLAlchemy>=1.2"],
+    install_requires=["starlette>=0.12.9", "SQLAlchemy>=1.2", "fastapi>=0.52.0"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
